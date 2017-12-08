32816d4f879572e5b248359442f0712e4f23cbbd
richard <richard@androidminds.cn>
2017-12-08 15:26:23 +0800

    Fix bug to let Zuul work well.

--- Zuul/src/main/java/cn/androidminds/springcloudstudy/ZuulApplication.java
+++ Zuul/src/main/java/cn/androidminds/springcloudstudy/ZuulApplication.java
@@ -3,12 +3,13 @@ package cn.androidminds.springcloudstudy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 
 @EnableZuulProxy
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class ZuulApplication {
 
 	public static void main(String[] args) {

50dcbe82be19812fb82ca65dc4a9b5e6c1245eef
richard <richard@androidminds.cn>
2017-12-09 15:12:29 +0800

    Add ConfigClient in Consumer project.

--- Consumer/src/main/java/cn/androidminds/springcloudstudy/ConsumerController.java
+++ Consumer/src/main/java/cn/androidminds/springcloudstudy/ConsumerController.java
@@ -1,6 +1,7 @@
 package cn.androidminds.springcloudstudy;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,5 +15,14 @@ public class ConsumerController {
     public @ResponseBody
     String sayHi(){
         return "Consumer : " + consumerService.hiService();
+    }
+
+    @Value("${web.server.port}")
+    String configPort;
+
+    @RequestMapping("/config")
+    public @ResponseBody
+    String getConfig(){
+        return "Consumer Config: " + configPort;
     }
 }

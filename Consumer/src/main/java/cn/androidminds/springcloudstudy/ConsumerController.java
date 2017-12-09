633b22a32924c6adedb97debc8da428941a36598
richard <richard@androidminds.cn>
2017-12-09 17:00:57 +0800

    using message bus in consumer project.

--- Consumer/src/main/java/cn/androidminds/springcloudstudy/ConsumerController.java
+++ Consumer/src/main/java/cn/androidminds/springcloudstudy/ConsumerController.java
@@ -2,11 +2,13 @@ package cn.androidminds.springcloudstudy;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ConsumerController {
     @Autowired
     ConsumerService consumerService;
